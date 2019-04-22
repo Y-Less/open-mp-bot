b18bfc8c7bd2471a116e3ce1c13dceb6a601afc3
Y-Less <git@y-less.com>
2019-04-22 18:58:12 +0200

    Don't clobber the case.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = (robot) => {
   robot.on('issues.opened', async (context) => {
     const config = await context.config('autolabel.yml')
-    const title = context.payload.issue.title.toLowerCase();
+    const title = context.payload.issue.title;
+    const cmp = title.toLowerCase();
 
     for (const l of config.labels) {
-      if (title.startsWith('[' + l.toLowerCase() + ']')) {
+      if (cmp.startsWith('[' + l.toLowerCase() + ']')) {
         return context.github.issues.update(context.issue({
           title: title.substr(l.length + 2).trim(),
           labels: [ l ],
